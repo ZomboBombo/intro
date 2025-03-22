2ef6f918a53059a9382fbc8f0aee3ce94247383a
Сергей Новиков <sergey.novikov.zb@gmail.com>
2025-03-22 15:44:37 +0700

    chore: added '_drafts/' folder

---
+++ _drafts/skills-pie/init-skills-pie.ts
@@ -0,0 +1,28 @@
+/**
+ * [__DRAFT__]
+ *
+ * Module full commented as draft.
+ *
+*/
+
+// import SkillsPie from '../modules/SkillsPie'
+
+// export default function initSkillsPie() {
+//   const skillsPieCanvas = document.getElementById('js-skills-pie-canvas') as HTMLCanvasElement
+
+//   if (!skillsPieCanvas) {
+//     return
+//   }
+
+//   const skillsPie = new SkillsPie(skillsPieCanvas, {
+//     data: [
+//       { name: 's1', value: 10, color: 'red' },
+//       { name: 's2', value: 20, color: 'green' },
+//       { name: 's3', value: 30, color: 'blue' },
+//       { name: 's4', value: 40 },
+//       { name: 's5', value: 50 },
+//       { name: 's6', value: 60 },
+//     ]
+//   })
+//   skillsPie.init()
+// }
