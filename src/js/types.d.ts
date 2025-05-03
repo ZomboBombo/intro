d441f04b5067da2192987ad3f647222c6af4fddd
Sergey Novikov <sergey.novikov.zb@gmail.com>
2025-05-03 18:41:09 +0700

    chore: added new types, fixed old ones

--- src/js/types.d.ts
+++ src/js/types.d.ts
@@ -6,7 +6,8 @@
 */
 export type NullishString = string | null
 export type NullishHTMLElem = HTMLElement | null
-export type NullishButton = HTMLButtonElement | HTMLElement | null
+export type NullishButton = HTMLButtonElement | null
+export type NullishHTMLElemOrButton = HTMLElement | HTMLButtonElement | null
 
 /**
  * When 'undefined' can be returned:
@@ -14,12 +15,14 @@ export type NullishButton = HTMLButtonElement | HTMLElement | null
 */
 export type UndefinedishString = string | undefined
 export type UndefinedishHTMLElem = HTMLElement | undefined
-export type UndefinedishButton = HTMLButtonElement | HTMLElement | undefined
+export type UndefinedishButton = HTMLButtonElement | undefined
+export type UndefinedishHTMLElemOrButton = HTMLElement | HTMLButtonElement | undefined
 
 /**
  * When 'null | undefined' can be returned:
  * @type {null | undefined}
 */
-export type UndefNullishString = string | null | undefined
-export type UndefNullishHTMLElem = HTMLElement | null | undefined
-export type UndefNullishButton = HTMLButtonElement | HTMLElement | null | undefined
+export type UndefNullishString = string | undefined | null
+export type UndefNullishHTMLElem = HTMLElement | undefined | null
+export type UndefNullishButton = HTMLButtonElement | undefined | null
+export type UndefNullishHTMLElemOrButton = HTMLElement | HTMLButtonElement | undefined | null
