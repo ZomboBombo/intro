b5868a743c9b26df526722156ce6cf1aa349e478
Sergey Novikov <sergey.novikov.zb@gmail.com>
2025-05-05 00:32:34 +0700

    chore: added 'imagemin' config
    CHANGELOG:
    – (chore:packages) added 'imagemin' packages;
    – (chore:packages) added 'preview' script;
    – (chore:config) added 'imagemin' config;

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,10 @@ import vituum from 'vituum'
 import pages from 'vituum/plugins/pages'
 import pug from '@vituum/vite-plugin-pug'
 import autoprefixer from 'autoprefixer'
+import viteImagemin from '@vheemstra/vite-plugin-imagemin'
+import imageminWebp from 'imagemin-webp'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminPngquant from 'imagemin-pngquant'
 
 // Custom Plugins
 import customStackSvgPlugin from './custom-vite-plugins/custom-stack-svg-plugin'
@@ -43,6 +47,22 @@ export default {
     customStackSvgPlugin({
       pathToSpriteIcns: './public/img/icons/',
       output: './public/img/sprite.svg',
+    }),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg({ quality: 80 }),
+        jpeg: imageminMozjpeg({ quality: 80 }),
+        png: imageminPngquant({ quality: [0.8, 0.9] }),
+      },
+      makeWebp: {
+        formatFilePath: (file) => `${file.slice(0, file.lastIndexOf('.'))}.webp`,
+        skipIfLargerThan: false,
+        plugins: {
+          jpg: imageminWebp({ quality: 80 }),
+          jpeg: imageminWebp({ quality: 80 }),
+          png: imageminWebp({ quality: 80 }),
+        },
+      },
     }),
     vituum(),
     pug({
