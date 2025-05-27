9118b981921796cd2e0e16a7664eb1dfe41a037a
Sergey Novikov <48211673+ZomboBombo@users.noreply.github.com>
2025-05-27 21:03:51 +0700

    [27.05.2025] Features: added «FSD» setup (#23)
    * chore: removed redundant 'assets'
    * features: added 'FSD' setup
    
    ---------
    
    Co-authored-by: Sergey Novikov <sergey.novikov.zb@gmail.com>

--- _vue/vite.config.ts
+++ _vue/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     open: true,
     port: 9000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   css: {
     preprocessorOptions: {
