defdec1db4997e7df8ae9efdb11d18299094058f
Sergey Novikov <sergey.novikov.zb@gmail.com>
2025-06-01 21:48:51 +0700

    features: added 'Def-Pages' base setup

---
+++ _vue/src/pages/sitemap/ui/Sitemap.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/">Home</a>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+
+</style>
