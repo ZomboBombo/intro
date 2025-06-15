92e1d854aee22affe4d698dbb342c5d1e934e59b
Sergey Novikov <sergey.novikov.zb@gmail.com>
2025-06-16 01:20:35 +0700

    [WIP] features: 'UI-kit' styles and logic (v1)

--- _vue/src/pages/ui-kit/data/ui-title-data.ts
+++ _vue/src/pages/ui-kit/data/ui-title-data.ts
@@ -1,29 +1,29 @@
 import type { CodeExamplesDataT } from '../config'
 
 export const UiTitleData: CodeExamplesDataT = {
-  default: {
-    descr: 'Default <UiTitle> component usage',
+  Default: {
+    descr: 'Default <UiTitle> component usage.',
     usage: '<UiTitle text="UiTitle" />',
     output: '<h1 class="ui-title ui-title--h1">UiTitle</h1>',
   },
-  lvlH2: {
-    descr: '',
+  '`lvl` attr': {
+    descr:
+      'The `lvl` component\'s attribute usage example. `lvl` can be "h1 – h6" levels – just like default HTML headings.',
     usage: '<UiTitle text="UiTitle" lvl="h2" />',
-    output: '',
+    output: '<h2 class="ui-title ui-title--h2">UiTitle</h2>',
   },
-  notHeading: {
-    descr: '',
+  '`not-heading` attr': {
+    descr:
+      'The `not-heading` attr will make component usage is "not-heading". Instead of the <h1 / h6> tag you\'ll get the <strong> by default.',
     usage: '<UiTitle text="UiTitle" not-heading />',
-    output: '',
+    output: '<strong class="ui-title">UiTitle</strong>',
   },
-  tagSpan: {
-    descr: '',
-    usage: '<UiTitle text="UiTitle" tag="span" />',
-    output: '',
-  },
-  tagDiv: {
-    descr: '',
-    usage: '<UiTitle text="UiTitle" tag="div" />',
-    output: '',
+  '`tag` attr': {
+    descr:
+      'The `tag` attr will make component with your HTML-tag you want it to be. If you use the `tag` attr you can omit the `not-heading` attr.',
+    usage:
+      '<UiTitle text="UiTitle" tag="span" /> or <UiTitle text="UiTitle" tag="span" /> or any other valid HTML-tag.',
+    output:
+      '<span class="ui-title">UiTitle</span> / <div class="ui-title">UiTitle</div>',
   },
 }
--- _vue/src/pages/ui-kit/ui/UiKit.vue
+++ _vue/src/pages/ui-kit/ui/UiKit.vue
@@ -1,33 +1,79 @@
 <script setup lang="ts">
   import UiTitle from '@entities/ui-title'
   import { UiTitleCodeExamples } from '../data'
+
+  const onCopyCodeUsage = (e: MouseEvent): void => {
+    e.preventDefault()
+
+    const copybtn = e.currentTarget as HTMLButtonElement
+    const copybtnDefaultTextContent = copybtn.textContent
+    const target = copybtn.parentElement!.querySelector('code')
+
+    if (!target) {
+      return
+    }
+
+    const treatedTargetText = target.textContent!.trim()
+
+    navigator.clipboard.writeText(treatedTargetText)
+
+    copybtn.textContent = 'Copied!'
+    copybtn.setAttribute('disabled', '')
+
+    setTimeout(() => {
+      copybtn.textContent = copybtnDefaultTextContent
+      copybtn.removeAttribute('disabled')
+    }, 1800)
+  }
 </script>
 
 <template>
-  <UiTitle text="UI-kit" />
+  <UiTitle text="UI-kit" is-centered-horizontal />
 
   <section class="ui-kit-section">
-    <UiTitle text="<UiTitle> component" lvl="h2" />
+    <h2 class="ui-kit-section__title">The &lt;UiTitle&gt; component</h2>
 
     <section
       v-for="[key, { descr, usage, output }] in UiTitleCodeExamples"
       :key="key"
+      class="ui-kit-subsec"
     >
-      <hgroup>
-        <UiTitle :text="key" lvl="h3" />
-        <p>{{ descr }}</p>
+      <hgroup class="ui-kit-subsec__heading">
+        <h3 class="ui-kit-subsec__title">{{ key }}</h3>
+        <p class="ui-kit-subsec__descr">{{ descr }}</p>
       </hgroup>
 
-      <pre>
-        <code>
-          {{ usage }}
-        </code>
-        <code>
-          {{ output }}
-        </code>
-      </pre>
+      <div class="ui-kit-subsec__example">
+        <strong class="ui-kit-subsec__example-title">Usage:</strong>
+
+        <pre class="ui-kit-subsec__example-demo">
+          <code>
+            {{ usage }}
+          </code>
+
+          <button
+              type="button"
+              class="ui-kit-subsec__example-copybtn"
+              title="Click to copy `usage fragment` to clipboard"
+              aria-label="Click to copy `usage fragment` to clipboard"
+              @click="onCopyCodeUsage"
+          >Copy</button>
+        </pre>
+      </div>
+
+      <div class="ui-kit-subsec__example">
+        <strong class="ui-kit-subsec__example-title">Output:</strong>
+
+        <pre class="ui-kit-subsec__example-demo">
+          <code>
+            {{ output }}
+          </code>
+        </pre>
+      </div>
     </section>
   </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  @forward './styles';
+</style>
