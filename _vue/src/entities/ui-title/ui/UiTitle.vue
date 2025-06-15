323b147c10cab0daedc3afb61393a44f0808d160
Sergey Novikov <sergey.novikov.zb@gmail.com>
2025-06-16 01:18:59 +0700

    features: added <UiTitle> 'is-centered-horizontal'

--- _vue/src/entities/ui-title/ui/UiTitle.vue
+++ _vue/src/entities/ui-title/ui/UiTitle.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
   import type { UiTitleI } from '@entities/ui-title/config'
 
-  const { notHeading = false, tag, lvl = 'h1' } = defineProps<UiTitleI>()
+  const {
+    notHeading = false,
+    isCenteredHorizontal,
+    tag,
+    lvl = 'h1',
+  } = defineProps<UiTitleI>()
 
   const definedTag = tag ?? 'strong'
   const definedComponent = notHeading || tag ? definedTag : lvl
   const headingLevelCssMod = notHeading || tag ? '' : `ui-title--${lvl}`
+  const centeredHorizontalCssMod = isCenteredHorizontal
+    ? 'ui-title--centered-horizontal'
+    : ''
 </script>
 
 <template>
   <component
     :is="definedComponent"
-    :class="['ui-code', 'ui-title', headingLevelCssMod]"
+    :class="[
+      'ui-code',
+      'ui-title',
+      headingLevelCssMod,
+      centeredHorizontalCssMod,
+    ]"
   >
     {{ text }}
   </component>
