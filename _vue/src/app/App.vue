b1acc0dbbf58062fe4e0d0c638fa702c22959919
Sergey Novikov <sergey.novikov.zb@gmail.com>
2025-06-23 22:13:57 +0700

    features: stylized inner Nav

--- _vue/src/app/App.vue
+++ _vue/src/app/App.vue
@@ -8,8 +8,13 @@
 </script>
 
 <template>
-  <nav>
-    <RouterLink v-if="$route.name !== SITEMAP_NAME" :to="SITEMAP_PATH">
+  <nav :class="$style['goto-nav']">
+    <RouterLink
+      v-if="$route.name !== SITEMAP_NAME"
+      :to="SITEMAP_PATH"
+      :class="$style['goto-nav__link']"
+      aria-label="Go to the «Sitemap» page"
+    >
       {{ SITEMAP_NAME }}
     </RouterLink>
   </nav>
@@ -19,4 +24,34 @@
   </AppLayoutSetup>
 </template>
 
-<style lang="scss"></style>
+<style module lang="scss">
+  .goto-nav {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    padding: var(--gap-site-container-def);
+
+    &__link {
+      $t-props: color, background-color, opacity;
+      @include trans-config($t-props);
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.3rem 1.2rem;
+      font-size: 2.4rem;
+      color: var(--ui-secondary);
+      border: 0.2rem solid var(--ui-secondary);
+      border-radius: 0.75em;
+
+      &:where(:hover, :focus-visible) {
+        color: var(--ui-primary);
+        background-color: var(--ui-secondary);
+      }
+
+      &:active {
+        opacity: 0.75;
+      }
+    }
+  }
+</style>
